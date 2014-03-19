b0810149fb66cbee09b4967d69ba6d01f219b219
Anders Ullnaess <andersu@mesan.no>
2014-03-19 23:17:08 +0100

    Fixed loading of board. Added a test ad at the bottom of the screen

--- src/main/java/no/lundesgaard/sudokufeud/sudokufeud_android/ui/BoardFragment.java
+++ src/main/java/no/lundesgaard/sudokufeud/sudokufeud_android/ui/BoardFragment.java
@@ -89,10 +89,19 @@ public class BoardFragment extends Fragment {
         square7.setOnItemClickListener(onFieldClickListener);
         square8.setOnItemClickListener(onFieldClickListener);
         square9.setOnItemClickListener(onFieldClickListener);
-        // Create an ad.
+
+        createAd();
+    }
+
+    private void createAd() {
         adView = new AdView(getActivity());
         adView.setAdSize(AdSize.BANNER);
         adView.setAdUnitId(AD_UNIT_ID);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(-1, -1);
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        layoutParams.addRule(RelativeLayout.BELOW, R.id.radioGroupTiles);
+        adView.setLayoutParams(layoutParams);
 
         // Add the AdView to the view hierarchy. The view will have no size
         // until the ad is loaded.
@@ -110,10 +119,20 @@ public class BoardFragment extends Fragment {
     }
 
     @Subscribe
-    public void setFieldValues(BoardFetchedEvent boardFetchedEvent) {        
+    public void setFieldValues(BoardFetchedEvent boardFetchedEvent) {
+        List<Integer> board = boardFetchedEvent.getBoard();
+
         for (int i = 0; i < squareAdapters.size(); i++) {
+            List<Integer> squareValues = new ArrayList<Integer>();
+            int start = (i/3)*18 + i*3;
+            List<Integer> squareRow1 = board.subList(start, start + 3);
+            List<Integer> squareRow2 = board.subList(start + 9, start + 12);
+            List<Integer> squareRow3 = board.subList(start + 18, start + 21);
+            squareValues.addAll(squareRow1);
+            squareValues.addAll(squareRow2);
+            squareValues.addAll(squareRow3);
             SquareAdapter squareAdapter = squareAdapters.get(i);
-            squareAdapter.setFieldValues(boardFetchedEvent.getBoard().subList(i*9, (i+1)*9));
+            squareAdapter.setFieldValues(squareValues);
         }
     }
 
